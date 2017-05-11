de0e20bb486d9b8ccbd1f77fc29a8c250b210d3e
Mounzer HADDARA <mounzer.haddara@hotmail.fr>
2017-05-11 16:22:37 +0200

    3eme commit : interface burger stable

--- app/burgers.controller.js
+++ app/burgers.controller.js
@@ -1,84 +1,95 @@
-import {
-    uniq,
-    clone
-} from 'lodash'
-import angular from 'angular'
+import { uniq, clone } from 'lodash'
 import uuid from 'uuid/v4'
 
 export class BurgersController {
-    constructor() {
-        this.burgers = [{
-                name: 'maxi b',
-                toppings: ['steak', 'salad'],
-                creator: 'paul@ici.fr'
-            },
-            {
-                name: 'zombie',
-                toppings: ['steak', 'tomate'],
-                creator: 'paul@ici.fr'
-            },
-            {
-                name: 'hot',
-                toppings: ['steak', 'tomate', 'spicy sauce'],
-                creator: 'paul@ici.fr'
-            },
-            {
-                name: 'vegan',
-                toppings: ['soja steak', 'salad', 'onions'],
-                creator: 'paul@ici.fr'
-            }
+  constructor () {
+    this.burgers = [
+      {
+        id: 1,
+        name: 'maxi b',
+        toppings: ['steak', 'salad'],
+        creator: 'paul@ici.fr'
+      },
+      {
+        id: 2,
+        name: 'hot',
+        toppings: ['steak', 'tomate', 'spicy sauce'],
+        creator: 'paul@ici.fr'
+      },
+      {
+        id: 3,
+        name: 'vegan',
+        toppings: ['soja steak', 'salad', 'onions'],
+        creator: 'paul@ici.fr'
+      },
+      {
+        id: 4,
+        name: 'zombie',
+        toppings: ['steak', 'tomate'],
+        creator: 'paul@ici.fr'
+      }
+    ]
 
-        ]
-        this.col = 'name'
-        this.desc = false
-        //definition de notre nouveau burger avec ces parametres 
-        this.newburger = this._initBurger()
+    this.col = 'name'
+    this.desc = false
 
+    this.newburger = this._initBurger()
+  }
+
+  getToppings () {
+    return uniq(this.burgers
+    .reduce((acc, {toppings}) => [...acc, ...toppings], []))
+
+    /* <=>
+    .reduce((acc, burger) => {
+      let toppings = burger.toppings
+      return acc.concat(toppings)
+    }, [])
+    */
+  }
+
+  sortBy (col) {
+    if (this.col !== col) this.desc = false
+    else this.desc = !this.desc
+    this.col = col
+  }
+
+  applySort (burger) {
+    if (this.col === 'name') return burger.name
+    if (this.col === 'toppings') return burger.toppings.length
+  }
+
+  save (form) {
+    if (form.$invalid) return
+
+    if (!this.newburger.id) {
+      // creation burger
+      this.newburger.id = uuid()
+      this.burgers.push(clone(this.newburger))
+    } else {
+      // modification burger
+      let idx = this.burgers.findIndex(b => b.id === this.newburger.id)
+      if (idx !== -1) {
+        this.burgers[idx] = clone(this.newburger)
+      }
     }
 
-    getToppings() {
-        return uniq(this.burgers
-            // ici on utilise un reduce(qui utilise un accumulateur)
-            // ici [...accumulateur, ...toppings] va permettre de prendre a chaque chaque donnée du tableau toppings 
-            //  et le stocker dans le tableau accumulateur en concatenant(voir l'explication dans le cahier)
-            .reduce((accumulateur, {
-                toppings
-            }) => [...accumulateur, ...toppings], []))
-        /* <=>
-.reduce((acc, burger) => {
- let toppings = burger.toppings
- return acc.concat(toppings)
-}, [])
-*/
+    this.newburger = this._initBurger()
+
+    // reset form state
+    form.$setUntouched()
+  }
+
+  editBurger (burger) {
+    this.newburger = burger
+  }
+
+  _initBurger () {
+    return {
+      id: null,
+      name: '',
+      toppings: [],
+      creator: ''
     }
-    // sortby permet d'inverser les ligne de nos colonnes
-    sortBy(col) {
-        if (this.col !== col) this.desc = false
-        else this.desc = !this.desc
-        this.col = col
-    }
-
-    getSortBase(burger) {
-        if (this.col === 'name') return burger.name
-        if (this.col === 'toppings') return burger.toppings.length
-    }
-
-
-    save(form) {
-
-        if (!form.$valid) return
-        // On clone le burger et on sauvegarde les toppings
-        this.burgers.push(clone(this.newburger))
-    }
-
-    // on met le _ avant le nom de la fonction pour la lisibilité de lecture et faire comprendre que c'est une sorte de methode private
-    _initBurger() {
-        return {
-            id: null,
-            name: '',
-            toppings: [],
-            creator: ''
-        }
-    }
-
+  }
 }
