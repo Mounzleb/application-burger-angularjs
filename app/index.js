aa7a8981fa81e899aa98064c14cb0fb942788dff
Mounzer HADDARA <mounzer.haddara@hotmail.fr>
2017-05-16 12:03:50 +0200

    5eme commit bis :timer fonctionnant et se re initialise

--- app/index.js
+++ app/index.js
@@ -1,18 +1,22 @@
 import angular from 'angular'
 import 'bootstrap/dist/css/bootstrap.css'
 
+import CountdownModule from './countdown'
+
 import { BurgerService } from './burger.service'
 import { ToppingService } from './topping.service'
-
 import { GameComponent } from './game/game.component'
 import { ToppingsComponent } from './toppings/toppings.component'
 import { BurgerComponent } from './burger/burger.component'
 import { RecipeComponent } from './recipe/recipe.component'
 
+
 // import { ReverseFilter } from './reverse.filter'
 
 // Création du module app (application)
-angular.module('app', [])
+// faire attention au fait que lorsqu'on declare un module il faut les declarer à la suite les uns des autre pour une meme appliacation
+// exemple de declaration de plusieur module pour une même application  :angular.module('app', [ Module1, Module2, Module3 .... ]) on peux egalement n'avoir pas de module specifique angular.module('app', [])
+angular.module('app', [ CountdownModule ])
 
 .component('dtaGame', GameComponent)
 .component('dtaToppings', ToppingsComponent)
--- app/recipe/recipe.component.js
+++ app/recipe/recipe.component.js
@@ -2,30 +2,27 @@ import template from './recipe.component.html'
 
 class controller {
 
-    constructor ($interval) {
-        this.$interval = $interval
+    constructor ($timeout) {
+        this.$timeout = $timeout
+        this.flag = false
     }
 
     $onChanges (changes) {
         if (changes.toppings && changes.toppings.currentValue) {
-            
             // on retourne le tableau (cloné) avant de l'afficher
             this.toppings = angular.copy(this.toppings).reverse();
-
-            // nouvelle recette === nouveau compteur
-            if (this.interval) {
-                this.$interval.cancel(this.interval)
-            }
-            this.cpt = 10
-            this.interval = this.$interval(() => {
-                this.cpt--
-                console.log(this.cpt)
-                if (this.cpt === 0) {
-                    this.$interval.cancel(this.interval)
-                    this.onTimeout()
-                }
-            }, 1000)
+            // reset timer
+            this.flag = !this.flag
+            this.time = 20 //{ value: 5 }
         }
+    }
+
+    end () {
+        console.log('end')
+    }
+
+    start (remain) {
+        console.log(remain)
     }
 
 }
